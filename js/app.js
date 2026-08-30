cdfdf1c456dca97bd1a996b3e13d23e891daba87
MarrapodiStefano <stef976@gmail.com>
2026-08-30 22:24:29 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
+// ==========================================
+// LA MIA SPESA
+// Cervello principale dell'app
+// ==========================================
 
+console.log("La mia spesa è stata avviata!");
