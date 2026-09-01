ec196614f6d91b67fcfcbf3e406e6d3ddf4d95c1
MarrapodiStefano <stef976@gmail.com>
2026-09-01 22:21:59 +0200

    V1.4.18 - Aggiorna service worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      const registration = await navigator.serviceWorker.register("./sw.js?v=27", { updateViaCache: "none" });
+      const registration = await navigator.serviceWorker.register("./sw.js?v=29", { updateViaCache: "none" });
       await registration.update();
 
       if (registration.waiting) {
