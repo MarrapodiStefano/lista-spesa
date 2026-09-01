f6843b0bca024905a470f74a8451906e85f295b0
MarrapodiStefano <stef976@gmail.com>
2026-09-01 20:20:24 +0200

    V1.4.1: aggiorna cache offline

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "lista-spesa-offline-v12";
+const CACHE_NAME = "lista-spesa-offline-v13";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=12",
-  "./js/app.js?v=12",
-  "./manifest.json?v=12"
+  "./css/style.css?v=13",
+  "./js/app.js?v=13",
+  "./manifest.json?v=13"
 ];
 
 self.addEventListener("install", event => {
