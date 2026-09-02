a36ffdbc2eaebee4cf309ad565c60b7f465c1312
MarrapodiStefano <stef976@gmail.com>
2026-09-02 08:02:58 +0200

    Corregge aggiornamento Service Worker della PWA

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      const registration = await navigator.serviceWorker.register("./sw.js?v=60", { updateViaCache: "none" });
+      const registration = await navigator.serviceWorker.register("./sw.js?v=73", { updateViaCache: "none" });
       await registration.update();
 
       if (registration.waiting) {
